01ef12a33fc9d81f517180f80abda0ef7f00b8d5
Diogo Marzano Chaves <diogomarzano@gmail.com>
2021-04-21 02:21:44 +0000

    Install ojdbc8 example

--- install_oracle.sh
+++ install_oracle.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
-echo "Instalando JDBC Oracle..."
+echo "Downloading ojdbc..."
 mkdir ${ORACLE_HOME}/lib
 cd ${ORACLE_HOME}/lib
 curl -L https://download.oracle.com/otn-pub/otn_software/jdbc/211/ojdbc8-full.tar.gz | tar zx --strip-components 1
-echo "JDBC Oracle instalado!"
+echo "Done!"
 
-echo "Inserindo ojdbc8.jar no Jboss..."
+echo "Send ojdbc8.jar to Jboss..."
 mkdir -p /opt/jboss/keycloak/modules/system/layers/base/com/oracle/jdbc/main/driver/
 cp ${ORACLE_HOME}/lib/ojdbc8.jar /opt/jboss/keycloak/modules/system/layers/base/com/oracle/jdbc/main/driver/ojdbc.jar
-echo "Feito!"
+echo "Done!"
